d1ff63b016e8df8230c976a89445f5debf25d338
chicongpham235 <chicongpham235@gmail.com>
2023-02-01 13:13:45 +0700

    fix

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ cd dist/
 git init 
 git add -A
 git commit -m "deploy"
-git push -f https://github.com/chicongpham235/dashboard2.git master:gh-pages
+git push -f https://github.com/chicongpham235/investors.git master:gh-pages
